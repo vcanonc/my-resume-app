66048512685064f0d43b0dd177cf71659245cd3d
Victor <vcanonc@unal.edu.co>
2024-10-30 11:56:31 -0500

    debug = True

--- resume/settings.py
+++ resume/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!fwx9$l+z@m3g*u4o4cuz0h)mlft1=pfi+(&zn@uqy9a5gde=m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
